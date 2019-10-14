af9d9146e3dbcef10f0c0762762a934b21aee174
Goel-Rishabh <goelrishabh99@gmail.com>
2019-10-14 15:18:56 +0530

    android studio basic structure

---
+++ abesProject/app/src/main/java/com/example/abeshackathon/Retro.java
@@ -0,0 +1,52 @@
+package com.example.abeshackathon;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Retro {
+
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder().readTimeout(60, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS);
+
+    public static OkHttpClient client = new OkHttpClient();
+
+    public static OkHttpClient.Builder getHttpClient() {
+        return httpClient;
+    }
+
+    public static void setClient() {
+        client = httpClient.build();
+    }
+
+    public static void setBuilder() {
+        builder.client(client);
+    }
+
+    static Gson gson = new GsonBuilder()
+            .setLenient()
+            .create();
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl("http://10.42.0.223:8080/")
+                    .addConverterFactory(GsonConverterFactory.create(gson));
+
+    private static Retrofit.Builder builder2;
+
+    public static <S> S createService(Class<S> serviceClass) {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        httpClient.addInterceptor(logging);
+        Retrofit retrofit = builder.client(httpClient.build()).build();
+        return retrofit.create(serviceClass);
+    }
+
+}
